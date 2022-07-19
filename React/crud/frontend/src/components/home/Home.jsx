ca035e9cb39fddaea3e30ab8a91495d0b75feabe
MayconTrix <maycon.tiutrix@gmail.com>
2022-07-19 17:05:18 -0300

    React/Crud

---
+++ React/crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props =>
+    <Main icon='home' title='Início'
+    subtitle='Segundo Projeto do capítulo de React.'>
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className='mb-0'>Sistema para exemplificar a construção de uma cadastro desenvolvido em React!</p>
+        <hr />
+        <p className='mb-0'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione maxime laboriosam consequatur necessitatibus iste,<br/> debitis alias nesciunt hic ipsum doloremque quasi unde explicabo blanditiis sapiente. Repellat aspernatur provident fugiat hic?!</p>
+    </Main>
---
+++ React/crud/frontend/src/components/templates/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        Desenvolvido com <i className='fa fa-heart text-danger'></i> por
+        <strong> Maycon<span className='text-danger'>T</span>rix</strong>
+        
+    </footer>
