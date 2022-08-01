0cc21419cda59a4f946cf9a1d8403633b868de10
MayconTrix <maycon.tiutrix@gmail.com>
2022-08-01 16:30:14 -0300

    Aulao NextJS e React

---
+++ Aulao-Nextjs/fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,10 @@
+import Cabecalho from '../components/Cabecalho'
+
+export default function Exemplo() {
+    return (
+        <div>
+            <Cabecalho titulo='Next.Js & React' />
+            <Cabecalho titulo='Aprende Next na prática' />
+        </div>
+    )
+}
---
+++ Aulao-Nextjs/fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,21 @@
+export default function Jsx() {
+//   const a = 4
+//   const b = 3
+//   console.log(a * b)
+
+//   const obj = { nome: "João", idade: 30 };
+
+const titulo = <h1>JSX é um Conceito Central</h1>
+
+function subtitulo() {
+    return <h2>{"muito legal".toUpperCase()}</h2>
+}
+
+  return (
+    <div>
+      {titulo}
+      {subtitulo()}
+      <p>{JSON.stringify({ nome: "João, idade: 30" })}</p>
+    </div>
+  )
+}
