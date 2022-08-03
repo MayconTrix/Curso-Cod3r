307cae7a86f785bf241468f9986645aa289fc42e
MayconTrix <maycon.tiutrix@gmail.com>
2022-08-03 16:47:49 -0300

    Mais Aulão NextJS e React

---
+++ Aulao-Nextjs/fundamentos/src/pages/estado.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Layout from '../components/Layout'
+
+export default function Estado() {
+    const [numero, setNumero] = useState(0) // React Hooks
+    
+    function incrementar() {
+        setNumero(numero + 1)
+    }
+    function decrementar() {
+        setNumero(numero - 1)
+    }
+
+    return (
+        <Layout titulo="Componente com Estado">
+            <div>{numero}</div>
+            <button onClick={incrementar}>Incrementar</button>
+            <button onClick={decrementar}>Decrementar</button>
+        </Layout>
+    )
+}
